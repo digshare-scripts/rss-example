9bae86634450dcd5728595a5f4aac2a2fb9c0f39
vilicvane <vilicvane@live.com>
2022-04-07 16:34:24 +0800

    初步实现

--- src/script.ts
+++ src/script.ts
@@ -1,22 +1,63 @@
 import {script} from '@digshare/script';
+import RSSParser from 'rss-parser';
 
-import fetch from 'node-fetch';
-import * as Cheerio from 'cheerio';
+// 配置目标 RSS 地址。
+const RSS_URL = 'https://vane.life/rss/';
 
-interface Payload {}
+const rssParser = new RSSParser();
 
-interface Storage {}
+interface Storage {
+  seen: string[];
+}
 
-export default script<Payload, Storage>(async (payload, {storage}) => {
-  // 使用 fetch 获取页面内容。
-  // let html = await fetch('https://www.example.com').then(response =>
-  //   response.text(),
-  // );
+export default script<void, Storage>(async (_payload, {storage}) => {
+  // 取出之前就看到过的订阅内容 id，将其转化为 Set 数据结构，便于后续判断。
+  const seenSet = new Set(storage.getItem('seen'));
 
-  // 使用 Cheerio 解析 HTML 内容。
-  // let $ = Cheerio.load(html);
+  // 使用 RSS Parser 加载、解析目标 RSS 列表。
+  const feed = await rssParser.parseURL(RSS_URL);
+
+  // 从加载的 RSS 订阅内容中筛选出之前没有看到过的内容。
+  const unseenItems = feed.items.filter(
+    item =>
+      // seenSet 中如果有 item.guid，就说明之前看到过。
+      !seenSet.has(item.guid!),
+  );
+
+  if (unseenItems.length === 0) {
+    // 如果没有发现新内容，中断脚本。
+    return;
+  }
+
+  // 把新内容的 id 加入看到过订阅内容 id 列表中。
+  storage.setItem(
+    'seen',
+    [
+      // 之前看到过的：
+      ...seenSet,
+      // 新内容的：
+      ...unseenItems.map(item => item.guid!),
+      // 只保留最近 10000 条记录：
+    ].slice(-10000),
+  );
+
+  // 从新内容中取出前 3 条（主要怕一次性新内容太多，比如脚本第一次执行）。
+  const mostRecentUnseenItems = unseenItems.slice(0, 3);
 
   return {
-    content: '这是一条价值不菲的消息！',
+    content: `${feed.title} 发布了 ${unseenItems.length} 篇新内容。`,
+    links: [
+      // 提供链接：
+      ...mostRecentUnseenItems.map(item => {
+        return {
+          // 链接标题使用订阅内容标题
+          title: item.title!,
+          // 链接描述使用订阅内容片段
+          description: item.contentSnippet!,
+          // 链接地址
+          url: item.link!,
+        };
+      }),
+    ],
   };
 });
